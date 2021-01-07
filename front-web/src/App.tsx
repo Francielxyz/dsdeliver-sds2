5694498eb2e48b2942234a243ac8e301d6c9914d
Francielxyz <ruanvha15@gmail.com>
2021-01-07 16:14:08 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
   return (
     <div >
       <h1>
         <NavBar/>
+        <Home />
       </h1>
     </div>
   );
