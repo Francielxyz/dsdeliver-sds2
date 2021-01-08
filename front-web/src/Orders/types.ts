71f20ef842c9667d51db224647535e5cc7f8e955
Francielxyz <ruanvha15@gmail.com>
2021-01-08 11:57:42 -0300

    Search order location

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -7,3 +7,9 @@ export type Product = {
     description: string;
     imageUri: string;
 }
+
+export type OrderLocationdata = {
+    latitude: number;
+    longitude: number;
+    address: string;
+}
